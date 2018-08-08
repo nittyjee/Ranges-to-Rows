df0567ce3ef96113055bbb5b0763d327bef69110
nittyjee <nittyjee@gmail.com>
2018-08-07 22:23:46 -0500

    Add files via upload

---
+++ createOriginal_2.sql
@@ -0,0 +1,54 @@
+﻿CREATE SCHEMA ng;
+
+SET search_path = ng, pg_catalog;
+--SET search_path = pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 270 (class 1259 OID 17431)
+-- Name: original; Type: TABLE; Schema: ng; Owner: postgres; Tablespace: 
+--
+
+DROP TABLE IF EXISTS original CASCADE;
+
+CREATE TABLE original
+(
+    Uniq integer NOT NULL,
+    Name integer NOT NULL
+
+);
+
+copy original from 'D:\ollie_maps_testing\exported_csv\ollie_maps_csv.csv' delimiter as ',' csv header;
+--copy original from 'D:\original_table.csv' delimiter as ',' csv header;
+--copy original from 'D:\retry.csv' delimiter as ',' csv header;
+
+
+-- Renaming 2 columns
+ALTER TABLE original RENAME COLUMN Name TO original;
+--ALTER TABLE original RENAME COLUMN Uniq TO uniq;
+
+
+--
+-- TOC entry 2249 (class 0 OID 17431)
+-- Dependencies: 270
+-- Data for Name: original; Type: TABLE DATA; Schema: ng; Owner: postgres
+--
+
+--
+-- TOC entry 2141 (class 2606 OID 17435)
+-- Name: original_pkey; Type: CONSTRAINT; Schema: ng; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY original
+    ADD CONSTRAINT original_pkey PRIMARY KEY (uniq, original);
+
+
+-- Completed on 2018-02-07 22:16:03 EET
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ years2.sql
@@ -0,0 +1,80 @@
+﻿with "table6" as (select
+	o."uniq",
+	y.*
+	from
+	ng."original" o
+--	"original" o
+
+	join (
+		select
+			row_number() over() seq,
+			"original" "start_date1",
+			lead("original") over() "end_date1"
+		from (
+			select
+				"original"
+			from
+				ng."original"
+--				"original"
+			group by
+				"original"
+			order by
+				"original"
+		) g
+	) y on y.start_date1 = o."original"
+	order by
+	"uniq",
+	"original"
+)
+select
+	"uniq",
+	"start_date",
+	"end_date"
+from
+(
+	select 
+		"uniq", "dmin", "dmax",
+		min(case when "first" = "seq" then "start_date1" else null end) "start_date",
+		max(case when "last" = "seq" then "end_date1" else null end) "end_date"
+	from (
+		select
+			m.*,
+			min("seq") over(partition by "uniq", "dmin", "dmax") "first",
+			max("seq") over(partition by "uniq", "dmin", "dmax" )  "last"
+			from
+			(
+				select
+					d.*,
+					min("diff") over (partition by "uniq" order by "seq") "dmin",
+					max("diff") over (partition by "uniq" order by "seq") "dmax"
+				from
+					(
+					select
+						t.*, 
+						coalesce("seq" - lag("seq") over(partition by "uniq" order by "seq"), 1) "diff"
+					from 
+						"table6" t
+					) d
+				order by
+					"uniq",
+					"seq"
+			) m
+			order by
+			"uniq",
+			"seq"
+	) g
+	group by 
+	"uniq", "dmin", "dmax"
+) f
+group by 
+	"uniq",
+	"start_date",
+	"end_date"
+order by 
+	"uniq",
+	"start_date"
+
+	
+
+--UPDATE original SET end_date = 1640
+--WHERE end_date IS NULL;
